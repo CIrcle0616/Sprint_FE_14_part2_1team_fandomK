fae8ec82486bac56c324e316f90b7cf3c14087f2
Circle0616 <happy_faces@naver.com>
2025-03-08 11:58:32 +0900

    feat: 가로형태의 아이돌 정보를 담은 컴포넌트를 추가했습니다

---
+++ src/components/IdolHorizontalCard.jsx
@@ -0,0 +1,36 @@
+import IdolCircle from "./IdolCircle";
+import IdolInfo from "./IdolInfo";
+import { styled } from "styled-components";
+
+const IdolHorizontalCardDiv = styled.div`
+  max-width: 100%;
+  height: 70px;
+  display: flex;
+  flex-grow: 1;
+  gap: 12px;
+  border: none;
+  background-color: #02000e;
+  & div {
+    display: flex;
+    align-items: center;
+    color: #f96d69;
+    font-size: 16px;
+    font-weight: 400;
+  }
+`;
+
+// const demoIdol = {
+//   name: "김채원",
+//   totalVotes: 200400,
+// };
+
+export default function IdolHorizontalCard({ idol, flex, idx }) {
+  const rank = idx + 1;
+  return (
+    <IdolHorizontalCardDiv>
+      <IdolCircle idol={idol} size={"70px"} />
+      <div>{rank}</div>
+      <IdolInfo idol={idol} flex={flex} />
+    </IdolHorizontalCardDiv>
+  );
+}
---
+++ src/components/IdolInfo.jsx
@@ -0,0 +1,45 @@
+import { styled } from "styled-components";
+
+const IdolInfoDiv = styled.div`
+  display: flex;
+  flex-grow: 1;
+  ${({ flex }) =>
+    flex === "col"
+      ? `
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 4px;
+  & div {
+  align-self: flex-start;}`
+      : `
+  flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    & div {
+      align-self: auto;
+    }
+  `}
+  & div {
+    display: flex;
+    font-size: 14px;
+  }
+  & div.bold {
+    font-weight: 500;
+    color: #ffffffde;
+  }
+  & div.light {
+    font-weight: 400;
+    color: #ffffff99;
+  }
+`;
+
+export default function IdolInfo({ idol, flex }) {
+  const { name, totalVotes } = { ...idol };
+  return (
+    <IdolInfoDiv flex={flex}>
+      <div className="bold">{name}</div>
+      <div className="light">{totalVotes + "표"}</div>
+    </IdolInfoDiv>
+  );
+}
