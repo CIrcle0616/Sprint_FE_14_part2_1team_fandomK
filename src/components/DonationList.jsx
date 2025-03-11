d963824209c0468e5d5b6df641196b0695b0d8dc
Circle0616 <happy_faces@naver.com>
2025-03-11 20:49:41 +0900

    feat: ref와 state를 이용해서 scroll시 왼쪽 패딩 값을 무시하는 코드 작성

--- src/components/DonationList.jsx
+++ src/components/DonationList.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Donation from "./Donation";
 import { fetchGetDonations } from "../utils/donationApi";
 import { styled } from "styled-components";
 
 const DonationFlexListWrap = styled.div`
   margin-top: 40px;
-  padding-left: 24px;;
+  padding-left: 24px;
   & h1 {
     font-size: 16px;
     font-weight: 700;
@@ -17,6 +17,17 @@ const DonationFlexList = styled.ul`
   display: flex;
   gap: 8px;
   margin-top: 16px;
+  overflow-x: auto;
+  -ms-overflow-style: none; //여기부터
+  scrollbar-width: none;
+  transition: margin-left 0.3s ease;
+  ${({ isScrolled }) =>
+    isScrolled &&
+    `
+  margin-left: -20px;`}
+  &::-webkit-scrollbar {
+    display: none; // 여기까지 스크롤바 없애는 속성
+  }
 `;
 
 const DonationLi = styled.li`
@@ -25,6 +36,16 @@ const DonationLi = styled.li`
 
 export default function DonationList() {
   const [donations, setDonations] = useState([]);
+  const [isScrolled, setIsScrolled] = useState(false); 
+  const scrollRef = useRef(null);
+
+  const handleScroll = () => {
+    if (scrollRef.current.scrollLeft > 0 && !isScrolled) {
+      setIsScrolled(true);
+    } else if (scrollRef.current.scrollLeft === 0 && isScrolled) {
+      setIsScrolled(false);
+    }
+  };
 
   useEffect(() => {
     const getDonationListData = async () => {
@@ -38,7 +59,11 @@ export default function DonationList() {
   return (
     <DonationFlexListWrap>
       <h1>후원을 기다리는 조공</h1>
-      <DonationFlexList>
+      <DonationFlexList
+        ref={scrollRef}
+        onScroll={handleScroll}
+        isScrolled={isScrolled}
+      >
         {donations.map((donation) => (
           <DonationLi key={donation.id}>
             <Donation donation={donation} />
