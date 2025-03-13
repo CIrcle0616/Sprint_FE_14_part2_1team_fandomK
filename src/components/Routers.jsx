d91059d39f5ec25247455e55a20e52cb9c1da749
Circle0616 <happy_faces@naver.com>
2025-03-13 22:04:11 +0900

    fix: 다시 재수정

--- src/components/Routers.jsx
+++ src/components/Routers.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "../pages/main/container/MainPage";
 import ListPage from "../pages/list/ListPage.jsx";
 import MyPage from "../pages/myPage/container/MyPage";
