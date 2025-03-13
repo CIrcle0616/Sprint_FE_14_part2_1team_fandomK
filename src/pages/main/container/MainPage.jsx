cdbf3fe4ac1df6351ab25a9ad45ce56f6b6baf2b
Circle0616 <happy_faces@naver.com>
2025-03-13 22:16:32 +0900

    fix: 메인페이지 수정 라우터 수정

--- src/pages/main/container/MainPage.jsx
+++ src/pages/main/container/MainPage.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LinkStyle = styled(Link)`
+  background-color: white;
+`;
+
 export default function MainPage() {
   return (
-    <>메인</>
+    <>
+      <LinkStyle to={"/list"}>목록 페이지</LinkStyle>
+      <LinkStyle to={"/mypage"}>마이 페이지</LinkStyle>
+      <LinkStyle to={"/addIdol"}>아이돌 추가 페이지</LinkStyle>
+    </>
   );
 }
