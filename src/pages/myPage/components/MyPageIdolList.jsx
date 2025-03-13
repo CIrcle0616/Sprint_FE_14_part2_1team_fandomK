fbe1cb3ec434cc9f4f8cfb97c29d8b1b1c2a81eb
Circle0616 <happy_faces@naver.com>
2025-03-13 21:44:14 +0900

    fix: 비동기 함수 파일 최상단에서 제거

--- src/pages/myPage/components/MyPageIdolList.jsx
+++ src/pages/myPage/components/MyPageIdolList.jsx
@@ -8,28 +8,11 @@ const TotalIdolList = styled.ul`
 `;
 
 //get http 요청으로 아이돌 불러오기
-const fetchMockIdol = async () => {
-  const response = await fetch(
-    "https://fandom-k-api.vercel.app/14-1/idols?pageSize=10"
-  );
-  if (!response.ok) return;
-  const data = await response.json();
-  const idols = data.list;
-  return idols;
-};
-const mockIdols = await fetchMockIdol();
-
 export default function SelectedIdol() {
   return (
     <div>
       <TotalIdolList>
-        {mockIdols.map((idol) => (
-          <li key={idol.id}>
-            <button type="button">
-              <IdolCircle idol={idol} />
-            </button>
-          </li>
-        ))}
+        
       </TotalIdolList>
     </div>
   );
