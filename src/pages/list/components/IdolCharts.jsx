6a6812fe7f30bae716b5917119e77f1d68cf7ad2
Circle0616 <happy_faces@naver.com>
2025-03-19 13:01:09 +0900

    아이돌 추가하기 페이지로 이동하는 링크 버튼 생성

--- src/pages/list/components/IdolCharts.jsx
+++ src/pages/list/components/IdolCharts.jsx
@@ -1,12 +1,14 @@
 import chartIcon from "../../../assets/icon/ic_chart.svg";
 import ChartTable from "./ChartTable";
 import { ChartWrapper } from "../styles/IdolChartsStyles";
+import { Link } from "react-router-dom";
 
 export default function IdolCharts({ openVoteModal, setChartGender }) {
   return (
     <ChartWrapper>
       <div className="header">
         <h1>이달의 차트</h1>
+        <Link to={'/addidol'} className="link">아이돌 추가하기</Link>
         <button onClick={openVoteModal}>
           <img src={chartIcon} />
           차트 투표하기
